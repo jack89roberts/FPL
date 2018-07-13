2977ee4615354d244379e16d8a5db1e184df0670
jackroberts89 <jackroberts1989@gmail.com>
2018-07-13 16:52:58 +0100

    api requests for fpl and fixture data, then convert to pandas df

--- import_data.py
+++ import_data.py
@@ -1,29 +1,82 @@
+#################
+### VARIABLES ###
+#################
+update_api = True
+fpl_file = 'json/fpl.json'
+fixtures_file = 'json/fixtures.json'
+token_file = 'api.token'
+
+###############
+### IMPORTS ###
+###############
 import requests
 import json
 import pandas as pd
 
-update_file = False
-file_name = 'raw.json'
-
-if update_file:
-    raw = requests.get('https://fantasy.premierleague.com/drf/bootstrap-static')
+#################
+### FUNCTIONS ###
+#################
+def get_json(url,headers=[]):
+    print('Getting',url,'...')
+    raw = requests.get(url, headers=headers)
     raw_json = raw.json()
-    
-    with open(file_name,'w') as dump_file:
-        json.dump(raw_json, dump_file)
+    return raw_json
+
+def save_json(raw_json, fname):
+    print('Saving',fname,'...')
+    with open(fname, 'w') as f:
+        json.dump(raw_json, f)
         
+def load_json(fname):
+    print('Loading',fname,'...')
+    with open(fname, 'r') as f:
+        raw_json = f.read()
+    
+    # convert to dict
+    raw_json = json.loads(raw_json)
+    
+    return raw_json
+  
+def json_to_df(raw_json,prefix='',suffix='.csv'):  
+    print('Converting json to data frames:')
+    for key in raw_json.keys():
+        print(key+'...', end='')
+        try:
+            df = pd.DataFrame(raw_json[key])
+            df.to_csv(prefix + key + suffix)
+            print('done.')
+        except:
+            print('ERROR!')
+            
+################
+### FPL DATA ###
+################            
+if update_api:
+    raw_json = get_json('https://fantasy.premierleague.com/drf/bootstrap-static')
+    save_json(raw_json, fpl_file)    
+    
 else:
-    with open(file_name, 'r') as dump_file:
-        raw_json = dump_file.read() 
+    raw_json = load_json(fpl_file)
 
-json_dict = json.loads(raw_json)
+print('------------------')
+json_to_df(raw_json,prefix='json/FPL_')
 
-for key in json_dict.keys():
-    print(key+'...', end='')
-    try:
-        df = pd.DataFrame(json_dict[key])
-        df.to_csv(key+'.csv')
-        print('done.')
-    except:
-        print('ERROR!')
+####################
+### FIXTURE DATA ###
+####################            
+print('------------------')
 
+if update_api:
+    with open(token_file,'r') as f:
+        token = f.read()
+        
+    raw_json = get_json('http://api.football-data.org/v2/competitions/2021/matches',
+                        headers={'X-Auth-Token':token})
+    
+    save_json(raw_json, fixtures_file)    
+    
+else:
+    raw_json = load_json(fixtures_file)
+
+print('------------------')
+json_to_df(raw_json,prefix='json/Fixtures_')
